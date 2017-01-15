bfb6d0e19c40ebc5f0b81bd44ec4a1ceb1c48b38
administrator <administrator@dl580g9-01.sqlcst.com>
2017-01-14 22:15:02 -0800

    Updating files to include the addition of specifying SQL Server host name

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo docker run -d --privileged --net=host \
-	-e "GRAPHITE_SERVER=${GRAPHITE_SERVER}" -e "GRAPHITE_PORT=${GRAPHITE_PORT}" \
-	-e "SQL_USERNAME=${SQL_USERNAME}" -e "SQL_PASSWORD=${SQL_PASSWORD}" \
-	--name collectd collectd 
+	-e "GRAPHITE_SERVER=127.0.0.1" -e "GRAPHITE_PORT=2003" \
+	-e "SQL_HOSTNAME=MyHostName" -e "SQL_USERNAME=MyUser" -e "SQL_PASSWORD=MyPassword" \
+	--name collectd stevecs38/collectd-sqlserver
