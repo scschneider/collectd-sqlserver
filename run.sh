33616cefd537ed66f1440b1068565eb54f772fba
root <root@dl560g8-01-v01.sqlcst.com>
2017-01-20 12:05:44 -0800

    Updated collectd to use the dbi interval. Before it was wrapped around double quotes and would default to pulling every 10 seconds.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 sudo docker run -d --privileged --net=host --pid=host -v /:/host:ro \
-	-e "GRAPHITE_SERVER=127.0.0.1" -e "GRAPHITE_PORT=2003" \
-	-e "SQL_HOSTNAME=MyHostName" -e "SQL_USERNAME=MyUser" -e "SQL_PASSWORD=MyPassword" -e "SQL_POLL_INTERVAL=5" \
-	--name collectd stevecs38/collectd-sqlserver
+        -e "GRAPHITE_SERVER=10.1.0.6" -e "GRAPHITE_PORT=2003" \
+        -e "SQL_HOSTNAME=dl560g8-01-v01" -e "SQL_USERNAME=sa" -e "SQL_PASSWORD=Washington!000" -e "SQL_POLL_INTERVAL=1" \
+        --name collectd stevecs38/collectd-sqlserver
+
