d44afcf26161e4eb3d8364a61eba7f5209ea0e21
scschneider <stschn@microsoft.com>
2017-02-22 08:39:23 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo docker run -d --privileged --net=host --pid=host -v /:/host:ro \
-        -e "GRAPHITE_SERVER=13.88.187.106" -e "GRAPHITE_PORT=20003" \
-        -e "SQL_HOSTNAME=stschn" -e "SQL_USERNAME=sa" -e "SQL_PASSWORD=sa" -e "SQL_POLL_INTERVAL=1" \
+        -e "GRAPHITE_SERVER=10.10.10.10" -e "GRAPHITE_PORT=2003" \
+        -e "SQL_HOSTNAME=HOSTNAME" -e "SQL_USERNAME=sa" -e "SQL_PASSWORD=sa" -e "SQL_POLL_INTERVAL=1" \
         --name collectd scschneider/collectd-sqlserver
 
